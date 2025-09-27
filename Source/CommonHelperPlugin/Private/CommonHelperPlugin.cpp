22322a0146b3fe209a55957fff7909d2da11e26f
qinxiao-1 <56571188+qinxiao-1@users.noreply.github.com>
2025-09-27 15:11:55 +0800

    first commit

---
+++ Source/CommonHelperPlugin/Private/CommonHelperPlugin.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CommonHelperPlugin.h"
+
+#define LOCTEXT_NAMESPACE "FCommonHelperPluginModule"
+
+void FCommonHelperPluginModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FCommonHelperPluginModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCommonHelperPluginModule, CommonHelperPlugin)
